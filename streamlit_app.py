c6f75a28170084df24681b1eb21365ac5eb56fc5
Abeermathur7 <66336010+Abeermathur7@users.noreply.github.com>
2024-05-30 17:05:23 +0000

    Added a title

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,14 +4,14 @@ import pandas as pd
 import altair as alt
 
 # Page title
-st.set_page_config(page_title='Interactive Data Explorer', page_icon='📊')
-st.title('📊 Interactive Data Explorer')
+st.set_page_config(page_title='Permit Data Exploration', page_icon='📊')
+st.title('📊 Permit Data Exploration')
 
 with st.expander('About this app'):
   st.markdown('**What can this app do?**')
-  st.info('This app shows the use of Pandas for data wrangling, Altair for chart creation and editable dataframe for data interaction.')
+  st.info('Interact with the permit dataset/Parquet files and explores each column.')
   st.markdown('**How to use the app?**')
-  st.warning('To engage with the app, 1. Select genres of your interest in the drop-down selection box and then 2. Select the year duration from the slider widget. As a result, this should generate an updated editable DataFrame and line plot.')
+  st.warning('Idk yet, this is a prototype')
   
 st.subheader('Which Movie Genre performs ($) best at the box office?')
 
